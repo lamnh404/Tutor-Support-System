7d732e7630149d2602bcef3c43bed07c3d67ce95
lamnh404 <lamcaonhat0404@gmail.com>
2025-09-15 11:09:48 +0700

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+     rules: {
+      // 🔹 Variables
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+
+      // 🔹 React
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+
+      // 🔹 Possible Errors / Best Practices
+      'no-console': 'warn',
+      'no-lonely-if': 'warn',
+      'no-trailing-spaces': 'warn',
+      'no-multi-spaces': 'warn',
+      'no-multiple-empty-lines': 'warn',
+      'no-unexpected-multiline': 'warn',
+
+      // 🔹 Stylistic Issues
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': ['warn', 'always'],
+      'indent': ['warn', 2],
+      'semi': ['warn', 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': ['warn', 'never'],
+      'linebreak-style': 'off',
+      'keyword-spacing': 'warn',
+      'comma-dangle': 'warn',
+      'comma-spacing': 'warn',
+      'arrow-spacing': 'warn',
+    },
+  },
+])
