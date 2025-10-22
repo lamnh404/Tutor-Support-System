b5a41d8a4ef8b732788ad0b56a8164f431b492a1
lamnh0404 <150221983+CaoNhatLam@users.noreply.github.com>
2025-10-22 13:49:52 +0700

    finish automatically call API refresh

--- src/utils/authorizedAxiosInstance.ts
+++ src/utils/authorizedAxiosInstance.ts
@@ -80,22 +80,21 @@ authorizedAxiosInstance.interceptors.response.use((res) => {
   //   return null
   //
   // }
-
+  console.log('Error response: ', error)
+  let errorMessage = error?.message
   const originalRequests = error.config
   // console.log('Original request: ', originalRequests)
   if (error.response?.status === 401 && error?.response?.data?.isExpired && originalRequests) {
     return handleTokenRefresh(originalRequests)
   }
   else if (error.response?.status === 401) {
+    if (error.response?.data?.title) {
+      errorMessage = error.response?.data?.title
+      toast.error(errorMessage)
+    }
     await userLogoutAPI(false)
     if (logoutFn) logoutFn()
     if (navigateFn) navigateFn('/login')
-    return Promise.reject(error)
-  }
-  let errorMessage = error?.message
-  if (error.response?.data?.title) {
-    errorMessage = error.response?.data?.title
-    toast.error(errorMessage)
     return Promise.reject(error)
   }
   console.error(error)
@@ -107,6 +106,11 @@ authorizedAxiosInstance.interceptors.response.use((res) => {
   else if (errorMessage=='Request failed with status code 401') errorMessage = 'Phiên đăng nhập đã hết hạn hoặc bạn chưa đăng nhập. Vui lòng đăng nhập lại.'
   else if (errorMessage=='Request failed with status code 400') errorMessage = 'Yêu cầu không hợp lệ - Vui lòng kiểm tra và thử lại.'
   else errorMessage = 'Lỗi không xác định - Vui lòng thử lại sau.'
+  if (error.response?.data?.title) {
+    errorMessage = error.response?.data?.title
+    toast.error(errorMessage)
+    return Promise.reject(error)
+  }
 
   toast.error(errorMessage)
 
